7e9c14519b5e556d4eee490df695987c8d60e941
Simon Vary <vary.simon@gmail.com>
2024-01-29 13:08:18 +0100

    updated init in solvers

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     "LandingGeneralizedStiefel",
     "RiemannianGeneralizedStiefel",
     "LandingCCA",
+    "LandingICA",
     "RiemannianRollingCCA",
      "compute_mean_std", 
      "loader_to_cov", 
@@ -18,5 +19,6 @@ from .riemannian_sd import RiemmGeneralizedStiefel
 from .landing_generalized_stiefel import LandingGeneralizedStiefel
 from .riemannian_generalized_stiefel import RiemannianGeneralizedStiefel
 from .landing_cca import LandingCCA
+from .landing_ica import LandingICA
 from .riem_rolling_cca import RiemannianRollingCCA
 
